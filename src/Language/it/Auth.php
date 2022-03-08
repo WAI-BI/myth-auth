e4a5a4c088a828f0feb5fab0b3814f1b76cc7e5e
szappino <szappino@gmail.com>
2022-03-08 11:10:14 +0100

    fix stringa di conferma attivazione

--- src/Language/it/Auth.php
+++ src/Language/it/Auth.php
@@ -20,7 +20,7 @@ return [
     // Activation
     'activationNoUser'          => 'Impossibile trovare un utente con questo codice di attivazione.',
     'activationSubject'         => 'Attiva il tuo account',
-    'activationSuccess'         => 'Conferma il tuo account facendo clic sul link di attivazione nell\'email che ti abbiamo inviato.',
+    'activationSuccess'         => 'Conferma il tuo account cliccando sul link di attivazione nell\'email che ti abbiamo inviato all\'indirizzo che ci hai indicato.',
     'activationResend'          => 'Invia ancora il messaggio di attivazione.',
     'notActivated'              => 'Questo account utente non è stato ancora attivato.',
     'errorSendingActivation'    => 'Impossibile inviare il messaggio di attivazione a: {0}',
