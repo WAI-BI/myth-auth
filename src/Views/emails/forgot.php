7e2d12285c1c6357692f7e49ffcdfe51d372b1ed
szappino <szappino@gmail.com>
2026-05-13 17:47:26 +0200

    Update password recovery message in forgot.php

--- src/Views/emails/forgot.php
+++ src/Views/emails/forgot.php
@@ -3,7 +3,7 @@
 </p>
 
 <p>
-    abbiamo ricevuto una richiesta di recupero della password per poter accedere alla piattaforma di Formazione per i corsi Colf, Badanti e Baby-sitter.
+    abbiamo ricevuto una richiesta di recupero della password per poter accedere alla piattaforma di AppContratti.
 </p>
 
 <p>Utilizza questo <a href="<?= site_url('reset-password') . '?token=' . $hash ?>">link</a> per procedere al ripristino della tua password.</p>
